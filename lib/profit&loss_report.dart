840f36a030296fab191848dd14401e972ba2eb02
Joseph Kay <kayjoseph049@gmail.com>
2026-02-18 12:01:13 +0300

    update on p&l report

--- lib/profit&loss_report.dart
+++ lib/profit&loss_report.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_pos/LoginPage.dart';
+import 'package:mobile_pos/customer.dart';
+import 'package:mobile_pos/expenses.dart';
+import 'package:mobile_pos/home.dart';
+import 'package:mobile_pos/products.dart';
+import 'package:mobile_pos/products_report.dart';
+import 'package:mobile_pos/sales_report.dart';
+import 'package:mobile_pos/settings.dart';
+import 'package:mobile_pos/supplier.dart';
 import 'sale.dart';
 import 'sales.dart';
 import 'package:pdf/widgets.dart' as pw;
@@ -178,6 +187,173 @@ class _ProfitLossReportState extends State<ProfitLossReport> {
             onPressed: items.isEmpty ? null : _generatePdf,
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home_filled, color: Colors.blue),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Home(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.inventory, color: Colors.blue),
+              title: Text('Products'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Products(),
+                  ),
+                );
+              },
+            ),
+
+            ListTile(
+              leading: Icon(Icons.shopping_cart, color: Colors.blue),
+              title: Text('Sales'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Sales(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.wallet, color: Colors.blue),
+              title: const Text('Expenses'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Expenses(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.local_shipping, color: Colors.blue),
+              title: Text('Suppliers'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CreateSupplierPage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.people, color: Colors.blue),
+              title: Text('Customers'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CreateCustomerPage(),
+                  ),
+                );
+              },
+            ),
+            // Reports Manager Dropdown
+            ExpansionTile(
+              leading: const Icon(Icons.folder_open, color: Colors.blue),
+              title: const Text('Reports Manager'),
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.inventory_2_outlined, color: Colors.blueAccent, size: 20,),
+                  title: const Text('Products Report', style: TextStyle(fontSize: 14),),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const ProductsValuationReport(),
+                      ),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.show_chart, color: Colors.blueAccent, size: 20,),
+                  title: const Text('Sales Report', style: TextStyle(fontSize: 14),),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SalesReport(),//SalesReportPage(),
+                      ),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.insert_chart_outlined, color: Colors.blueAccent, size: 20,),
+                  title: const Text('Profit & Loss Report', style: TextStyle(fontSize: 14),),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const ProfitLossReport(),
+                      ),
+                    );
+                  },
+                )
+              ],
+            ),
+            ListTile(
+              leading: Icon(Icons.settings, color: Colors.blue),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Settings(),
+                  ),
+                );
+              },
+            ),
+
+            ListTile(
+              leading: Icon(Icons.logout_outlined, color: Colors.red),
+              title: Text('Sign Out'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Loginpage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
       body: Column(
         children: [
